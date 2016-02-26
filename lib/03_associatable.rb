12d37245f8aff5f2a32915c22d52126c1119c4ce
PKLong <patrickklong@gmail.com>
2016-02-26 17:58:01 -0500

    upto associatable#has_many

--- lib/03_associatable.rb
+++ lib/03_associatable.rb
@@ -10,34 +10,49 @@ class AssocOptions
   )
 
   def model_class
-    # ...
+    class_name.constantize
   end
 
   def table_name
-    # ...
+    model_class.table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{name}Id".underscore.to_sym
+    self.class_name = options[:class_name] || name.to_s.singularize.camelcase
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+    self.foreign_key = options[:foreign_key] || "#{self_class_name}Id".underscore.to_sym
+    self.class_name = options[:class_name] || name.to_s.singularize.camelcase
+    self.primary_key = options[:primary_key] || :id
   end
 end
 
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+    define_method(name) do
+      fk = send(options.foreign_key)
+      pk = options.primary_key
+      options.model_class.where({pk => fk}).first
+    end
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, options[:class_name], options)
+    p options
+    define_method(name) do
+      fk = send(options.foreign_key)
+      pk = options.primary_key
+      options.model_class.where({fk => pk}).first
+    end
   end
 
   def assoc_options
