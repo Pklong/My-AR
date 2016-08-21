9d614a1a6c82555d21c061b7a0bffb3abfb92562
PKLong <patrickklong@gmail.com>
2016-08-21 17:29:56 -0400

    Consolidate files and upgrade Ruby version.

--- lib/associatable.rb
+++ lib/associatable.rb
@@ -1,4 +1,4 @@
-require_relative '02_searchable'
+require_relative 'searchable'
 require 'active_support/inflector'
 
 
@@ -59,6 +59,41 @@ module Associatable
     @assoc_options ||= {}
     @assoc_options
 
+  end
+end
+
+def has_one_through(name, through_name, source_name)
+
+  define_method(name) do
+    through_options = self.class.assoc_options[through_name]
+    source_options =
+      through_options.model_class.assoc_options[source_name]
+
+    through_table = through_options.table_name
+    through_pk = through_options.primary_key
+    through_fk = through_options.foreign_key
+
+    source_table = source_options.table_name
+    source_pk = source_options.primary_key
+    source_fk = source_options.foreign_key
+
+    through_fk_value = self.send(through_fk)
+
+    source_options.model_class.parse_all(
+    DBConnection.execute(<<-SQL, through_fk_value)).first
+    SELECT
+      #{source_table}.*
+    FROM
+      #{through_table}
+    JOIN
+      #{source_table}
+    ON
+      #{through_table}.#{source_fk} = #{source_table}.#{source_pk}
+    WHERE
+      #{through_table}.#{through_pk} = ?
+    LIMIT
+      1
+    SQL
   end
 end
 
